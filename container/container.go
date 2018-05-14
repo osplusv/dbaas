e0aa37a93159763238902747dced6edcab80f7ae
Osvaldo Villagrana <avillagrana@gmail.com>
2018-05-14 11:08:22 -0500

    Adding function to create a new docker database container

--- container/container.go
+++ container/container.go
@@ -2,8 +2,12 @@ package container
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 
+	"github.com/osplusv/dbaas/docker"
 	"github.com/osplusv/dbaas/util"
+	"github.com/pborman/uuid"
 )
 
 type DatabaseContainer struct {
@@ -20,4 +24,43 @@ type Repository interface {
 	FindAll() []*DatabaseContainer
 }
 
+type ContainerSpecification struct {
+	Image string
+}
+
+var currentPort = 55000
 var ErrUnknown = errors.New("unknown database")
+
+func NewDatabaseContainer(specifications ContainerSpecification) (*DatabaseContainer, error) {
+	container, err := allocateNewContainer(specifications.Image)
+	if err != nil {
+		return nil, err
+	}
+	return container, nil
+}
+
+func allocateNewContainer(image string) (*DatabaseContainer, error) {
+	d, err := docker.New()
+	if err != nil {
+		return nil, err
+	}
+
+	secret := strings.Split(strings.ToUpper(uuid.New()), "-")[0]
+	currentPort++
+	newHostPort := strconv.Itoa(currentPort)
+
+	containerID, err := d.CreateContainer(image, []string{
+		"MYSQL_ROOT_PASSWORD=" + secret,
+	}, newHostPort, secret)
+
+	return &DatabaseContainer{
+		ContainerID:      containerID,
+		Image:            image,
+		HostPort:         newHostPort,
+		DatabaseServices: 0,
+		EnvCredential: util.Credential{
+			Username: "root",
+			Password: secret,
+		},
+	}, nil
+}
