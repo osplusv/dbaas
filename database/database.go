05dc8a32cf5a968bd3ac578903f49459c04dd99e
Osvaldo Villagrana <avillagrana@gmail.com>
2018-05-14 10:50:17 -0500

    Adding database model

---
+++ database/database.go
@@ -0,0 +1,26 @@
+package database
+
+type (
+	Database struct {
+		ID               string
+		Name             string
+		Type             string
+		ConnectionString string
+		EnvCredential    util.Credential
+		UserID           string
+		ContainerID      string
+	}
+
+	DatabaseSpecification struct {
+		Type string
+	}
+)
+
+type Repository interface {
+	Store(cargo *Database) error
+	Find(id string) (*Database, error)
+	FindAll() []*Database
+	Delete(id string) error
+}
+
+var ErrUnknown = errors.New("unknown database")
